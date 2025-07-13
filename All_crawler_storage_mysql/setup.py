2b4ed55d4088beefc50147db40f3538d96260cbc
wade <w24810@gmail.com>
2025-07-14 05:00:06 +0800

    修改一下內容

--- All_crawler_storage_mysql/setup.py
+++ All_crawler_storage_mysql/setup.py
@@ -14,8 +14,8 @@ setup(
     description="test",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
-    author="group4",  # Optional
-    author_email="group4@gmail.com",  # Optional
+    author="wade",  # Optional
+    author_email="w24810@gmail.com",  # Optional
     classifiers=[  # Optional
         "Development Status :: 3 - Alpha",
     ],
